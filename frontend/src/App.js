756b3a1208031c053bce2770116b2f1e11ead13f
mslinux09@gmail.com <mslinux09@gmail.com>
2026-07-29 19:01:08 +0530

    added content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>This application is running inside Kubernetes.</p>
+        <p>This is app1 being managed by manoj.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
